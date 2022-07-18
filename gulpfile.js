1ccb86a1e3bc344449286820c19587a03fc6ce38
azamatdevs <shomuratovazamat116@gmail.com>
2022-07-18 22:42:37 +0500

    Configured gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,48 @@
-const {src, dest} = require('gulp')
-const sass = require('gulp-sass')
+const {src, dest, series, watch} = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
 const csso = require('gulp-csso')
 const include = require('gulp-file-include')
 const del = require('del')
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
 const htmlmin = require('gulp-htmlmin')
-const sync = require('browser-sync')
+const sync = require('browser-sync').create()
 
 
 function html(){
-    src('src/**.html')
+    return src('src/**.html')
         .pipe(include({
             prefix: '@@'
         }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(dest('dist'))
 }
+
+function scss(){
+    return src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(csso())
+        .pipe(concat('index.css'))
+        .pipe(dest('dist'))
+}
+
+function clear(){
+    return del('dist')
+}
+
+function serve(){
+    sync.init({
+        server: 'dist'
+    })
+
+    watch('src/**.html', series(html)).on('change', sync.reload)
+    watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+}
+
+exports.build = series(clear, scss, html)
+exports.serve = series(clear, scss, html, serve)
